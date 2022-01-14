43d24acd69e2d2b5073a024b28fb5dc3b50a0b99
Andreyrvs <cortaesai@gmail.com>
2022-01-14 17:13:49 -0300

    Requisito: 1.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
       </BrowserRouter>
     );
   }
