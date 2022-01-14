751ef19b71edfe196d9a827fb4e416732a3858d8
Andreyrvs <cortaesai@gmail.com>
2022-01-14 17:15:00 -0300

    Requisito: 1.3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './pages/Album';
 import Login from './pages/Login';
 import Search from './pages/Search';
 
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Route path="/" component={ Login } />
         <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
       </BrowserRouter>
     );
   }
