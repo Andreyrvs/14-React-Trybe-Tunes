12859d042c2256b895ac20ad4238400f3f367b65
Andreyrvs <cortaesai@gmail.com>
2022-01-14 17:20:37 -0300

    Requisito: 1.6

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Login from './pages/Login';
 import Search from './pages/Search';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/search" component={ Search } />
         <Route path="/favorites" component={ Favorites } />
         <Route path="/profile" component={ Profile } />
+        <Route path="/" component={ Login } />
       </BrowserRouter>
     );
   }
