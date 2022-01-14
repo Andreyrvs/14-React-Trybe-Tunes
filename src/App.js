f0797b2ef9a27813182986cb87a81f0fe292bb83
Andreyrvs <cortaesai@gmail.com>
2022-01-14 17:24:33 -0300

    Requisito: 1.7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="/search" component={ Search } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/" component={ Login } />
+        <Switch>
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route exact path="/" component={ Login } />
+          <Route path="/*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
