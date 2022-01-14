81c0728b9b4d3ecddf9081229a008f39cd1ef5fe
Andreyrvs <cortaesai@gmail.com>
2022-01-14 17:26:18 -0300

    Requisito: 1 - Finalizado

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ class App extends React.Component {
           <Route path="/search" component={ Search } />
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile" component={ Profile } />
-          <Route path="/*" component={ NotFound } />
           <Route exact path="/" component={ Login } />
+          <Route path="/*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
     );
