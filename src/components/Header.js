d1b75950881bb9ccc42b98d7b6a5de609b928349
Andreyrvs <cortaesai@gmail.com>
2022-01-15 15:19:32 -0300

    Requisito: 3 - Finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { userName } = this.props;
     return (
       <div>
         <header data-testid="header-component">
-          Pessoa Logada
+          <span data-testid="header-user-name">
+            {userName}
+          </span>
         </header>
       </div>
     );
   }
 }
+
+Header.propTypes = {
+  userName: PropTypes.string,
+}.isRequire;
 
 export default Header;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
+import './css/search.css';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.receiveAPI = this.receiveAPI.bind(this);
+    this.state = {
+      userName: '',
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.receiveAPI();
+  }
+
+  async receiveAPI() {
+    this.setState({
+      isLoading: true,
+    });
+    const resolve = await getUser();
+    this.setState({
+      userName: resolve.name,
+      isLoading: false,
+    });
+  }
+
   render() {
+    const { isLoading, userName } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-        <div>
-          <p>Rota Search</p>
-        </div>
+      <div data-testid="page-search" className="search-page">
+        {isLoading ? <Loading /> : (
+          <>
+            <Header userName={ userName } />
+            <div>
+              <p>Rota Search</p>
+            </div>
+          </>
+        )}
       </div>
     );
   }
