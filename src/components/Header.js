37b068b0093f06e42c0f9e7492d2f12ccfbec3bf
Andreyrvs <cortaesai@gmail.com>
2022-01-15 14:19:51 -0300

    Requisito: 3.5

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header data-testid="header-component">
+          Pessoa Logada
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
