2aee17cfff759486e8b800122981b8aa6620d653
Andreyrvs <cortaesai@gmail.com>
2022-01-17 17:31:28 -0300

    Restaura arquivo Album.js :happy:

--- src/pages/Album/Album.js
+++ src/pages/Album/Album.js
@@ -2,13 +2,66 @@ import React, { Component } from 'react';
 import Header from '../../components/Header';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.callAPI = this.callAPI.bind(this);
+    this.state = {
+      albumMusic: [],
+      isLoading: false,
+      albumLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  async callAPI() {
+    this.setState({
+      albumLoading: false,
+    });
+    const { match: { params: { id } } } = this.props;
+    const response = await getMusics(id);
+    this.setState({
+      albumMusic: response,
+      albumLoading: false,
+    });
+  }
+
   render() {
+    const { albumMusic, userName, isLoading, albumLoading } = this.state;
+    console.log(albumMusic);
     return (
       <div data-testid="page-album">
-        <Header />
+        {isLoading ? <Loading /> : (
+          <>
+            <Header userName={ userName } />
+            {albumLoading ? <Loading /> : (
+              <section className="album-description">
+                {albumMusic.map((album, index) => (
+                  index === 0
+                    ? (
+                      <section key={ album.collectionId }>
+                        <p data-testid="album-name">{album.albumName}</p>
+                        <p data-testid="artist-name">{album.artistName}</p>
+                      </section>)
+                    : (
+                      <section>
+                        <p>{album.trackName}</p>
+                      </section>
+                    )
+                ))}
+              </section>
+            )}
+          </>
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.object,
+};
 
 export default Album;
